bb75caa394339af9f1322806920a984d0cbccef6
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-08 16:54:06 +0100

    hero section

---
+++ src/component/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <>
+      <div className="w-[75%] pl-10 leading-10 my-5">
+        <p className="text-3xl font-semibold text-gray-50 py-6">Hello 👋</p>
+        <p className="text-5xl font-bold text-gray-100">
+          I'm<span className="text-gray-700"> Silvernus Ifoghale. </span>😉
+        </p>
+        <p className="py-5 text-gray-800 font-bold text-4xl ">
+          A Software Engineer. 😎
+        </p>
+        <p className="text-white font-semibold text-2xl">
+          Frontend Enthusiast 👌.
+        </p>
+        <p className="text-gray-100 text-sm pt-4">
+          I'm the developer you've been searching for - passionate, driven, and
+          ready to bring your projects to life. with a keen eye for detail and a
+          commitment to excellence, I'm dedicated to delivering top-notch design
+          solutions that exceed expectations. Let's collaborate and turn your
+          vision into reality!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { SiK3S } from "react-icons/si";
 import NavBar from "./NavBar";
+import Hero from "./Hero";
 
 export default function Portfolio() {
-  //Introduction/About Me -Briefly introduce yourself and your background.
+  //Introduction/About Me -Briefly introduce yourself and your background.✔️
   //Projects -Title, -Description, -Technologies used, -Link to the live demo,-Link to the GitHub repository
   //Skills- list skills you have acquired and improved upon during the challenge.
   //Experience/Reflections: Share your experience and any reflection of what you have learned and achieved.
@@ -11,9 +12,9 @@ export default function Portfolio() {
 
   return (
     <>
-      <div className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div className="w-full h-auto bg-gradient-to-r from-cyan-500 to-blue-500 ">
         <NavBar />
-        <div></div>
+        <Hero />
       </div>
     </>
   );
